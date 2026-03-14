3b783e7288ae6f8bbe303651858ae61cd054712b
iNothingAtAll <inaa.from_light@proton.me>
2026-03-13 22:07:15 -0500

    [add] se agregaron algunos modelos para los servicios de usuario

--- api/models.py
+++ api/models.py
@@ -8,3 +8,18 @@ class UserResponse(BaseModel):
     saldo: float
     
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    nombre: str
+    nombre_publico: str
+    telefono: str
+    correo: str
+    password_hash: str
+
+
+class UserUpdate(BaseModel):
+    nombre_publico: str | None = None
+    telefono: str | None = None
+    correo: str | None = None
+    password_hash: str | None = None
